b7ad670f70ba013f842d53d856eea574e75d17e4
Rafael Lins <rafael.lins777@gmail.com>
2025-07-19 22:32:00 -0300

    chore(first): first commit

---
+++ src/main/java/com/example/exemplo_solid/ExemploSolidApplication.java
@@ -0,0 +1,13 @@
+package com.example.exemplo_solid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExemploSolidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExemploSolidApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/exemplo_solid/ExemploSolidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exemplo_solid;
+
+import org.junit.jupiter.api.*;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemploSolidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
